4907000a48417201518098a2934b06b94d1f1363
CJBiohacker <cj.moyses@gmail.com>
2021-03-19 22:43:04 -0300

    "Exercícios do Carro 2 - Construtores e Instanciação de Objetos"

---
+++ Projetos em JAVA/src/main/java/ExercicioCarro2/Carro.java
@@ -0,0 +1,50 @@
+package ExercicioCarro2;
+
+public class Carro {
+
+    public String Marca, Modelo, Ano, Variante;
+    /* Declarando os ATRIBUTOS em formato de variáveis do tipo String.
+    CONSTRUTOR SEM PASSAGEM DE PARÂMETROS */
+
+    public String getMarca() {
+        return Marca;
+    }
+    /* Definindo o MÉTODO para retorno da variável Marca através do comando getMarca. */
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+    /*Definindo o MÉTODO para configuração da variável Marca através do comando setMarca. */
+
+    public String getModelo() {
+        return Modelo;
+    }
+    /* Definindo o MÉTODO para retorno da variável Modelo através do comando getModelo. */
+
+    public void setModelo(String Modelo) {
+        this.Modelo = Modelo;
+    }
+    /*Definindo o MÉTODO para configuração da variável Modelo através do comando setModelo. */
+
+    public String getAno() {
+        return Ano;
+    }
+    /* Definindo o MÉTODO para retorno da variável Ano através do comando getAno. */
+
+    public void setAno(String Ano) {
+        this.Ano = Ano;
+    }
+    /*Definindo o MÉTODO para configuração da variável Ano através do comando getAno. */
+
+    public String getVariante() {
+        return Variante;
+    }
+    /* Definindo o MÉTODO para retorno da variável Variante através do comando getVariante. */
+
+    public void setVariante(String Variante) {
+        this.Variante = Variante;
+    }
+    /*Definindo o MÉTODO para configuração da variável Modelo através do comando getVariante. */
+
+}
+
---
+++ Projetos em JAVA/src/main/java/ExercicioCarro2/Ex1/InstanciaAtributoObjeto.java
@@ -0,0 +1,20 @@
+package ExercicioCarro2.Ex1;
+
+import ExercicioCarro2.Carro;
+
+public class InstanciaAtributoObjeto {
+
+    public static void main(String[] args) {
+
+        Carro carro = new Carro(); // Construção e Instanciação do OBJETO
+
+        carro.setMarca("Tesla"); // Trazendo o MÉTODO 'setMarca' para definir a variável 'Marca' do OBJETO 'carro'.
+        carro.setModelo("Model S"); // Trazendo o MÉTODO 'setModelo' para definir a variável 'Modelo' do OBJETO 'carro'.
+        carro.setAno("2019"); // Trazendo o MÉTODO 'setAno' para definir a variável 'Ano' do OBJETO 'carro'.
+
+        System.out.println(carro.getMarca() + " " + carro.getModelo() + " " + carro.getAno());
+        // Imprime na tela a 'Marca' através do MÉTODO 'getMarca'.
+
+    }
+
+}
