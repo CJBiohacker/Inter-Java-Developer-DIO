06fc2347000c0b3c617834e2492b35b00a437a4d
CJBiohacker <cj.moyses@gmail.com>
2021-03-30 23:06:32 -0300

    "Programação Orientada à Objetos"

--- Projetos em JAVA/src/main/java/ProgramacaoOrientadaObjetos/ExercicioHerançaPolimorfismo/Resultado.java
+++ Projetos em JAVA/src/main/java/ProgramacaoOrientadaObjetos/ExercicioHerançaPolimorfismo/Resultado.java
@@ -1,26 +1,26 @@
 package ProgramacaoOrientadaObjetos.ExercicioHerançaPolimorfismo;
 
-class Resultado {       //Corrigir casas decimais após a vírgula das taxas de imposto
+class Resultado {  //Corrigir casas decimais após a vírgula das taxas de imposto
 
     public static void main(String[] args) {
 
         CargoLoja gerente = new Gerente();
 
-        gerente.setNome("Fabio Mozine");
-        gerente.setSalario(8450.50);
-        gerente.setImposto(0.1);
+            gerente.setNome("Fabio Mozine");
+            gerente.setSalario(8450.50);
+            gerente.setImposto(0.1);
 
         CargoLoja supervisor = new Supervisor();
 
-        supervisor.setNome("Pedro");
-        supervisor.setSalario(4795.35);
-        gerente.setImposto(0.05);
+            supervisor.setNome("Pedro");
+            supervisor.setSalario(4795.35);
+            gerente.setImposto(0.05);
 
         CargoLoja atendente = new Atendente();
 
-        atendente.setNome("Jibóia");
-        atendente.setSalario(1250.30);
-        gerente.setImposto(0.01);
+            atendente.setNome("Jibóia");
+            atendente.setSalario(1250.30);
+            gerente.setImposto(0.01);
 
         System.out.println("|‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|");
         System.out.println("|             Lajä Records             |");
@@ -32,14 +32,12 @@ class Resultado {       //Corrigir casas decimais após a vírgula das taxas de 
         System.out.println("Nome: " + gerente.getNome());
         System.out.println("Salário: R$ " + String.format("%.2f",gerente.getSalario()));
         System.out.println("Taxa de Imposto: " + String.format("%.2f",gerente.getImposto()) + "%");
-        System.out.println("Valor Pago: R$ " + String.format("%.2f",gerente.calcImposto()));
-        System.out.println(" ");
+        System.out.println("Valor Pago: R$ " + String.format("%.2f",gerente.calcImposto()) + "\n");
 
         System.out.println("Nome: " + supervisor.getNome());
         System.out.println("Salário: R$ " + String.format("%.2f",supervisor.getSalario()));
         System.out.println("Imposto Pago: R$ " + String.format("%.3f",supervisor.getImposto()) + "%");
-        System.out.println("Valor Pago: R$ " + String.format("%.2f",supervisor.calcImposto()));
-        System.out.println(" ");
+        System.out.println("Valor Pago: R$ " + String.format("%.2f",supervisor.calcImposto()) +"\n");
 
         System.out.println("Nome: " + atendente.getNome());
         System.out.println("Salário: R$ " + String.format("%.2f",atendente.getSalario()));
